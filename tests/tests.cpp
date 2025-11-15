4ffa70580d371dd735bba77930f29dcd9c3bfa0a
agzatyagov <agzatyagov@gmail.com>
2025-11-15 07:39:50 +0300

    Add files via upload

---
+++ tests/tests.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "replacinfunc.h"
+
+TEST(ReplacinTest, Example1) {
+    EXPECT_EQ(replacinfunc("abcabc", 2, 'a', 'X'), "abcXbc");
+}
+
+TEST(ReplacinTest, NoReplacement) {
+    EXPECT_EQ(replacinfunc("qwerty", 3, 'z', 'Z'), "qwerty");
+    EXPECT_EQ(replacinfunc("test", 0, 't', 'T'), "test");
+}
+
+TEST(ReplacinTest, Numbers) {
+    EXPECT_EQ(replacinfunc("obamo aa oo", 2, 'a', 'o'), "obamo oa oo");
+}
+
+TEST(ReplacinTest, Special) {
+    EXPECT_EQ(replacinfunc("a-a-a-a", 1, '-', '_'), "a_a_a_a");
+}
+
+TEST(ReplacinTest, Edges) {
+    EXPECT_EQ(replacinfunc("", 5, 'x', 'y'), "");
+    EXPECT_EQ(replacinfunc("aaaa", 5, 'a', 'b'), "aaaa");
+}
